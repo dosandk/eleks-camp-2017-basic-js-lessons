fd6f5e0db8b65645e165dff0c66cfccd503876d2
Vitaliy Sokolov <sokolovvitalik@gmail.com>
2017-02-26 23:37:28 +0200

    Add number functions

--- Sokolov/src/main.js
+++ Sokolov/src/main.js
@@ -60,7 +60,55 @@ function countWordsUsingRegexp(str) {
   return wordCounter;
 }
 
+function multiTable(startsWith, numLines) {
+  if (typeof startsWith === 'undefined')
+    startsWith = 1;
+  if (typeof numLines === 'undefined')
+    numLines = 81;
+  var i, j,
+      lineCounter = 0,
+      result = "";
+  if (startsWith < 1
+      || startsWith >= 10
+      || numLines < 1)
+    return result;
+  for (i = startsWith; i < 10; i++) {
+    for (j = 1; j < 10; j++) {
+      result += i + " * " + j + " = " + i * j + ";\n";
+      lineCounter++;
+      if (lineCounter >= numLines) {
+        return result;
+      }
+    }
+    result += "\n==========\n\n";
+  }
+  return result;
+}
+
+function printMultiTable(startsWith, numLines) {
+  var content = multiTable(startsWith, numLines);
+  if (typeof global === 'object') {
+    console.log(content);
+  } else {
+    document.write('<div class="multi-table">');
+    document.write(content.replace(/\n/g, "<br />\n"));
+    document.write('</div>');
+  }
+}
+
+function doubleNum(num) {
+  var s = "" + num;
+  if (s.length % 2 === 0) {
+    var mid = s.length / 2;
+    if (s.substr(0, mid) == s.substr(mid, mid)) {
+      return num;
+    }
+  }
+  return num * 2;
+}
+
 // console.log(repeat("hello", 3));
 // console.log(turnMeBaby("hello"));
 // console.log(ucFirst("hello"));
 // console.log(power(5, 3));
+printMultiTable(5, 10);
--- Sokolov/test/main.spec.js
+++ Sokolov/test/main.spec.js
@@ -1,43 +1,85 @@
-describe("basic-js examples", function() {
-  it("should repeat string n times", function() {
-    expect(repeat('a', 5)).toBe('aaaaa');
-    expect(repeat('a', 0)).toBe('');
-    expect(repeat('hello', 2)).toBe('hellohello');
+describe("basic-js tasks:", function() {
+  describe("repeat(str)", function() {
+    it("should repeat string n times", function() {
+      expect(repeat('a', 5)).toBe('aaaaa');
+      expect(repeat('a', 0)).toBe('');
+      expect(repeat('hello', 2)).toBe('hellohello');
+    });
   });
 
-  it("should capitalize a word", function() {
-    expect(ucFirst("john")).toBe('John');
-    expect(ucFirst(" john")).toBe('John');
-    expect(ucFirst("вася")).toBe("Вася");
-    expect(ucFirst("")).toBe("");
+  describe("ucFirst(str)", function() {
+    it("should capitalize a word", function() {
+      expect(ucFirst("john")).toBe('John');
+      expect(ucFirst(" john")).toBe('John');
+      expect(ucFirst("вася")).toBe("Вася");
+      expect(ucFirst("")).toBe("");
+    });
   });
 
-  it("should reverse a string", function() {
-    expect(turnMeBaby("abc")).toBe("cba");
-    expect(turnMeBaby("Hello world")).toBe("dlrow olleH");
+  describe("turnMeBaby(str)", function() {
+    it("should reverse a string", function() {
+      expect(turnMeBaby("abc")).toBe("cba");
+      expect(turnMeBaby("Hello world")).toBe("dlrow olleH");
+    });
   });
 
-  it("should raise to the nth power", function() {
-    expect(raiseToPower(5, 2)).toBe(25);
-    expect(raiseToPower(10, 1)).toBe(10);
-    expect(raiseToPower(3, 0)).toBe(1);
+  describe("raiseToPower(num, power)", function() {
+    it("should raise to the nth power", function() {
+      expect(raiseToPower(5, 2)).toBe(25);
+      expect(raiseToPower(10, 1)).toBe(10);
+      expect(raiseToPower(3, 0)).toBe(1);
+    });
   });
 
-  it("should truncate a string", function() {
-    expect(truncate("foo bar", 3)).toBe("foo...");
-    expect(truncate("foo bar", 7)).toBe("foo bar");
+  describe("trancate(str, maxlength)", function() {
+    it("should truncate a string", function() {
+      expect(truncate("foo bar", 3)).toBe("foo...");
+      expect(truncate("foo bar", 7)).toBe("foo bar");
+    });
   });
 
-  it("should return number of words in the string", function() {
-    expect(countWords("foo bar")).toBe(2);
-    expect(countWords("foo")).toBe(1);
-    expect(countWords(" ")).toBe(0);
-    expect(countWords("\nfoo\n\t  bar  baz")).toBe(3);
+  describe("countWords*(str)", function() {
+    it("should return number of words in the string", function() {
+      expect(countWords("foo bar")).toBe(2);
+      expect(countWords("foo")).toBe(1);
+      expect(countWords(" ")).toBe(0);
+      expect(countWords("\nfoo\n\t  bar  baz")).toBe(3);
 
-    expect(countWordsUsingRegexp("foo bar")).toBe(2);
-    expect(countWordsUsingRegexp("foo")).toBe(1);
-    expect(countWordsUsingRegexp(" ")).toBe(0);
-    expect(countWordsUsingRegexp("\nfoo\n\t  bar  baz")).toBe(3);
-    expect(countWordsUsingRegexp("а б в")).toBe(3);
+      expect(countWordsUsingRegexp("foo bar")).toBe(2);
+      expect(countWordsUsingRegexp("foo")).toBe(1);
+      expect(countWordsUsingRegexp(" ")).toBe(0);
+      expect(countWordsUsingRegexp("\nfoo\n\t  bar  baz")).toBe(3);
+      expect(countWordsUsingRegexp("а б в")).toBe(3);
+    });
+  });
+
+  describe("multiTable()", function() {
+    it('should print the multiplication table', function() {
+      expect(multiTable()).toMatch(/^1\s*\*\s*1\s*=\s*1;/);
+      expect(multiTable()).toMatch(/9\s*\*\s*9\s*=\s*81;/);
+    });
+
+    it('should print a part of the multiplication table', function() {
+      expect(multiTable(2, 1)).toMatch(/^2 \* 1 = 2;/);
+      expect(multiTable(10, 1)).toBe('');
+      expect(multiTable(0, 1)).toBe('');
+      expect(multiTable(5, 10)).toMatch(/5 \* 5 = 25;/);
+    });
+  });
+
+  describe('doubleNum(num)', function() {
+    it('should multiply number by 2', function() {
+      expect(doubleNum(2)).toBe(4);
+      expect(doubleNum(6)).toBe(12);
+      expect(doubleNum(111)).toBe(222);
+      expect(doubleNum(2002)).toBe(4004);
+    });
+
+    it('should not multiply when repeated number', function() {
+      expect(doubleNum(66)).toBe(66);
+      expect(doubleNum(4343)).toBe(4343);
+      expect(doubleNum(100100)).toBe(100100);
+      expect(doubleNum(111111)).toBe(111111);
+    });
   });
 });
