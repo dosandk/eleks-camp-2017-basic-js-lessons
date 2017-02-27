6d5eb82c028df8c3100f65da0e0ca4f2efc0acd7
Andrii Vasylytsia <andrii.vasylytsia@gmail.com>
2017-02-27 16:24:24 +0200

    Added Strings tasks 4 and 5; Changed structure

--- solutions/solution.js
+++ solutions/solution.js
@@ -1,4 +1,5 @@
 // Strings
+console.log('__Strings:')
 
 // 1
 function multiStrings (str, count) {
@@ -21,7 +22,7 @@ function ucFirst(str) {
 
 ucFirst("вася");
 
-//3
+// 3
 function truncate(str, maxlength) {
 	if (str.length > maxlength)
 		return (str.slice(0, maxlength) + '...');
@@ -31,3 +32,26 @@ function truncate(str, maxlength) {
 
 let task3 = truncate('abcdefghijklmnopQrstuVwxyz', 3);
 console.log('task3: ' + task3);
+
+// 4
+function turnMeBaby(str) {
+	return str.split('').reverse().join('');
+}
+
+let task4 = turnMeBaby('Abstract');
+console.log('task4: ' + task4);
+
+// 5
+function wordsCount (str) {
+	let string = str.replace(/\W/g, ' ');
+	let arr = string.split(' ');
+	arr = arr.filter(function (key) {
+		if (key != null) // 'null' - exclude spaces
+			return key;
+	});
+
+	
+	console.log('task5: ' +  arr.length);
+}
+
+wordsCount('   dsdsd dss d    - !   dsd sds  dsdsd   ');
