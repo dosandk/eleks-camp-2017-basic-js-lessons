c63d7604f8737083c93b53dd70eec55cc0538fb6
nazartsyhanyuk <nazartsyhanyuk@gmail.com>
2017-03-05 00:05:10 +0200

    deleted duplicate code

--- arrays/1.js
+++ arrays/1.js
@@ -1,48 +1,44 @@
 let ImmutableArray = {
-    push: (arr, el) => {
-        let tempArr = [];
-        arr.forEach((item=>{tempArr.push(item)}));
+    copy:  function (arr) {
+        return arr.slice();
+    },
+    push:  function (arr, el) {
+        let tempArr = this.copy(arr);
         tempArr.push(el);
         return tempArr;
     },
-    pop: (arr) => {
+    pop:  function (arr) {
         return arr.slice(0, arr.length-1);
     },
-    shift: (arr) => {
-        let tempArr = [];
-        arr.forEach((item=>{tempArr.push(item)}));
+    shift:  function (arr) {
+        let tempArr =this.copy(arr);
         tempArr.shift();
         return tempArr;
     },
-    unshift: (arr, el) => {
-        let tempArr = [];
-        arr.forEach((item=>{tempArr.push(item)}));
+    unshift:  function (arr, el) {
+        let tempArr =this.copy(arr);
         tempArr.unshift(el);
         return tempArr;
     },
-    remove: (arr, index) => {
-        let tempArr = [];
-        arr.forEach((item=>{tempArr.push(item)}));
+    remove:  function (arr, index) {
+        let tempArr =this.copy(arr);
         tempArr.splice(index, 1);
         return tempArr;
     },
-    insert: (arr, el, index) => {
-        let tempArr = [];
-        arr.forEach((item=>{tempArr.push(item)}));
+    insert:  function (arr, el, index) {
+        let tempArr =this.copy(arr);
         tempArr.splice(index, 0, el);
         return tempArr;
     },
-    swapWithPrevious: (arr, index) => {
-        let tempArr = [];
-        arr.forEach((item=>{tempArr.push(item)}));
+    swapWithPrevious:  function (arr, index) {
+        let tempArr =this.copy(arr);
         let element = tempArr[index];
         tempArr.splice(index, 1);
         tempArr.splice(index-1, 0, element);
         return tempArr;
     },
-    swapWithNext: (arr, index) => {
-        let tempArr = [];
-        arr.forEach((item=>{tempArr.push(item)}));
+    swapWithNext:  function (arr, index) {
+        let tempArr =this.copy(arr);
         let element = tempArr[index];
         tempArr.splice(index, 1);
         tempArr.splice(index+1, 0, element);
