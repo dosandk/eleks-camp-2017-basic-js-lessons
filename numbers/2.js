91a8998299f33615ddcc4afb034056750a54adf7
nazartsyhanyuk <nazartsyhanyuk@gmail.com>
2017-03-09 22:15:49 +0200

    fixed multitable

--- numbers/2.js
+++ numbers/2.js
@@ -1,9 +1,9 @@
 function multiTable() {
-    document.write(' <table border=1, cellpadding=0, cellspacing=0, width="90%">');
-    for (i = 1; i <= 9; i++) {
+    document.write('<table border=1, cellpadding=0, cellspacing=0, width="90%">');
+    for (let i = 1; i <= 9; i++) {
         document.write("<tr>");
 
-        for (k = 1; k <= 9; k++) {
+        for (let k = 1; k <= 9; k++) {
             document.write("<td>" + (k * i) + "</td>");
         }
 
