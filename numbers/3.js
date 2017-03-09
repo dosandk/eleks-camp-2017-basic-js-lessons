9e6b47cc1ef52e981f5ea8271ce68540439d6eb3
nazartsyhanyuk <nazartsyhanyuk@gmail.com>
2017-03-09 22:16:19 +0200

    fixed multitable(2)

--- numbers/3.js
+++ numbers/3.js
@@ -1,7 +1,7 @@
 function dynamicMultiTable(a, b) {
-    document.write(' <table border=1, cellpadding=0, cellspacing=0, width="90%">');
+    document.write('<table border=1, cellpadding=0, cellspacing=0, width="90%">');
     document.write("<tr>");
-    for (k = 1; k <= b; k++) {
+    for (let k = 1; k <= b; k++) {
         document.write("<td>" + (k * a) + "</td>");
     }
     document.write("</tr>");
